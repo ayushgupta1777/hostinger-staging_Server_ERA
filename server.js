fb07ab4bba487c247e53d985460152e33736abf0
aerospace141 <prakratigtn@gmail.com>
2026-02-17 21:33:25 +0530

    Port_change

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ app.use((req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const server = app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`📚 API Docs: http://localhost:${PORT}/api-docs`);
