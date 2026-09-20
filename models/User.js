a28549b0f1b7dc41a6bd07f44c35af62e6e1f560
Ayush Gupta <120300586+ayushgupta1777@users.noreply.github.com>
2026-09-20 13:55:19 +0530

    feat: add Mongoose User model with schema, authentication, and role management

--- models/User.js
+++ models/User.js
@@ -103,7 +103,7 @@ const userSchema = new mongoose.Schema({
 
 // Hash password before saving
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
+  if (!this.isModified('password') || !this.password) {
     return next();
   }
 
